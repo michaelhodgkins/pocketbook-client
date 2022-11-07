6a07f854955ec636c9348414ac54f90f4a20ddab
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-11-07 13:36:28 +0000

    basic style of nav bar added, with empty icons for light and dark mode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Disclosure } from '@headlessui/react'
+import { SunIcon } from '@heroicons/react/24/solid'
+import { MoonIcon } from '@heroicons/react/24/solid'
 const ResponsiveNavBar = () => {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-    <div className="relative flex h-16 items-center justify-around">
-    <button className="inline-flex items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 ">
-    <Link to={"/"}>Home</Link> </button>
-    <button className="inline-flex items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 ">
-    <Link to={"/landing"}>Landing Page</Link></button> 
-    <button className="inline-flex items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 ">
-    <Link to={"/creation"}>Create</Link></button>
-    <button className="inline-flex items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 ">
-    <Link to={"/profile"}>Profile</Link></button>
-    <button className="inline-flex items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 ">
-    <Link to={"/signup"}>Signup</Link></button>
-    <button className="inline-flex items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 ">
-    <Link to={"/login"}>Login</Link></button>
+    <div className="bg-gray-800 h-screen w-16 flex flex-col m-0 fixed top-0 items-center justify-around">
+    <button className="bg-gray-600 border border-white-700 text-white hover:bg-green-600 px-4 py-2 mt-2 mb-2 rounded-3xl hover:rounded-xl transition-all duration-200 ease-linear">
+    <Link to={"/"}>H</Link> </button>
+    <button className="bg-gray-600 items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 mt-2 mb-2 rounded-3xl hover:rounded-xl transition-all duration-200 ease-linear">
+    <Link to={"/landing"}>L</Link></button> 
+    <button className="bg-gray-600 items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 mt-2 mb-2 rounded-3xl hover:rounded-xl transition-all duration-200 ease-linear">
+    <Link to={"/creation"}>C</Link></button>
+    <button className="bg-gray-600 items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 mt-2 mb-2 rounded-3xl hover:rounded-xl transition-all duration-200 ease-linear">
+    <Link to={"/profile"}>P</Link></button>
+    <button className="bg-gray-600 items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 mt-2 mb-2 rounded-3xl hover:rounded-xl transition-all duration-200 ease-linear">
+    <Link to={"/signup"}>S</Link></button>
+    <button className="bg-gray-600 items-center border border-white-700 text-white hover:bg-green-600 px-4 py-2 mt-2 mb-2 rounded-3xl hover:rounded-xl transition-all duration-200 ease-linear">
+    <Link to={"/login"}>L</Link></button>
+    <SunIcon className='h-6 w-6 text-yellow-500' />
+    <MoonIcon className='h-6 w-6 text-white' />
     </div>
-    </Disclosure>
   )
 }
 
