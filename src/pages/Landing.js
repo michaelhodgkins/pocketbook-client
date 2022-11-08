86340355707d40b43e96efcc26fdbc00738b374c
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-11-08 16:46:02 +0000

    rough mesh layout of landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,7 +3,23 @@ import React from 'react'
 export const Landing = () => {
   return (
     <div>
-    <p> Splash screen</p>
+    <header className='bg-white shadow'>
+        <div className='mx-auto max-w-7xl py-2 px-2 text-center'>
+            <h1 className='text-2xl text-black-700'>PocketBook</h1>
+        </div>
+    </header>
+    <main>
+        <div className='mx-auto max-w-7xl py-2'>
+            <div className='bg-gray-300 px-2 py-2'>
+            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+            </div>
+        </div>
+        <div className= 'mx-auto max-w-7xl py-2'>
+        <div className='bg-gray-300 px-2 py-2'>
+            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+            </div>
+        </div>
+    </main>
     </div>
   )
 }
