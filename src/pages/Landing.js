59424c0d5db70357224649c225c765d782c3c6a2
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-11-25 17:25:41 +0000

    placement boxes added for content in landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,25 +2,37 @@ import React from "react";
 export const Landing = () => {
   return (
     <>
-      <div className="spacing">
-        <header className="bg-white-300 shadow w-full">
-          <div className="max-w-7xl py-2 px-2 text-center">
-            <h1 className="text-2xl text-black-700">PocketBook</h1>
+      <div className="spacing bg-yellow-200">
+        <header className="bg-white-300 shadow w-screen ">
+          <div className="py-2 px-2 ">
+            <h1 className="text-3xl text-black-700">PocketBook</h1>
           </div>
         </header>
-        <div className="flex justify-center">
-          <div className="max-w-7xl py-2 opacity-90 relative flex justify-center">
+        <div className="flex justify-center py-2">
+          <div className=" w-6/12 opacity-90 relative flex justify-center">
             <img src="./images/library.jpg" alt="library"></img>
-            <h2 className="absolute top-1 text-white p-4">
+            <h2 className="absolute text-white p-4">
               What will your next read be?
             </h2>
           </div>
         </div>
-        <div className="max-w-7xl py-2 ">
-          <div className=" bg-black px-2 py-2 flex items-center">
-            <div className="h-96 rounded-lg border-4 border-dashed border-blue-200 w-1/3" />
-            <div className="h-96 rounded-lg border-4 border-dashed border-green-200 w-1/3" />
-            <div className="h-96 rounded-lg border-4 border-dashed border-yellow-200 w-1/3" />
+        <div className="flex justify-center">
+          <div className="py-2 flex justify-around w-full ">
+            <div className="box-model grid">
+            <p className="text-white text-xl justify-self-center">This will be the first feature</p>
+            </div>
+            <div className="box-model flex items-center">
+              <p className="text-white">This will be the second feature</p>
+            </div>
+            <div className="box-model">
+              <p className="text-white">This will be the third feature</p>
+            </div>
+            <div className="box-model">
+              <p className="text-white">This will be the fourth feature</p>
+            </div>
+            <div className="box-model">
+              <p className="text-white">This will be the fifth feature</p>
+            </div>
           </div>
         </div>
       </div>
