608e80bbf4b368f7095764c3cb1f513142e221da
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-11-27 22:26:49 +0000

    update form added, fixed textarea bug when overflow was happening, fixed radio label placement, some css updates

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-const Create = () => {
+const Update = () => {
   return (
     <div className="w-1/2 flex pl-5 text-center">
       <form className="w-1/2 mt-2 bg-white shadow-md w-1/2 flex-col border py-4 min-w-fit">
-        <label className="block text-gray-600 text-md">Name of Book</label>
+        <label className="block text-gray-600 text-md">
+          Update name of book
+        </label>
         <input type="text" placeholder="" className="border w-1/2"></input>
-        <label className="block text-gray-600 text-md">Author/s</label>
+        <label className="block text-gray-600 text-md">Edit Author</label>
         <input type="text" placeholder="" className="border w-1/2"></input>
-        <label className="block text-gray-600 text-md">Rating</label>
+        <label className="block text-gray-600 text-md"> Adjust Rating</label>
         <input
           type="range"
           min="0"
@@ -16,10 +18,10 @@ const Create = () => {
           placeholder=""
           className="border w-1/2"
         />
-        <label className="block text-gray-600 text-md">Date read</label>
+        <label className="block text-gray-600 text-md">Change date read</label>
         <input type="date" placeholder="Please enter DOB" className="border" />
         <label className="block text-gray-600 text-md">
-          Would you recommend?
+          Edit recommendation ?
         </label>
 
         <input
@@ -37,8 +39,15 @@ const Create = () => {
           id="no"
         />
         <label htmlFor="no">No</label>
+
         <br />
-        <textarea className="border max-h-40 max-w-fit"rows={2} cols={30} placeholder='Please write your book review here...'/>
+        <label className="block text-gray-600 text-md">Revise review ?</label>
+        <textarea
+          className="border max-h-40"
+          rows={2}
+          cols={30}
+          placeholder="Please write your book review here..."
+        />
         <div className="flex justify-center p-10">
           <br />
           <button className="bg-grey-400 border w-20 hover:bg-green-500">
@@ -55,4 +64,4 @@ const Create = () => {
   );
 };
 
-export default Create;
+export default Update;
--- src/pages/Creation.js
+++ src/pages/Creation.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Create from '../components/Create'
-
+import SideBar from '../components/SideBar'
+import Update from '../components/Update'
 export const Creation = () => {
   return (
-    <div className='spacing h-screen bg-red-600'>
+    <div className=''>
+    <SideBar />
+    <div className='flex-col w-screen spacing h-screen bg-red-600'>
     <Create />
+    <Update />
+    </div>
     </div>
   )
 }
